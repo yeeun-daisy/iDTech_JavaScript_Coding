d155d8be78357040a0b2f47e24106f719c43cac2
yeeun-daisy <110549480+yeeun-daisy@users.noreply.github.com>
2022-08-06 01:44:15 +0900

    Add files via upload

---
+++ Project Pt 2-PacMen Factory Activity/pacmen.js
@@ -0,0 +1,78 @@
+const pacArray = [
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
+];
+const pacMen = []; // This array holds all the pacmen
+
+// This function returns an object with random values
+function setToRandom(scale) {
+  return {
+    x: Math.random() * scale,
+    y: Math.random() * scale,
+  };
+}
+
+// Factory to make a PacMan at a random position with random velocity
+function makePac() {
+  // returns an object with random values scaled {x: 33, y: 21} 
+  let velocity = setToRandom(10); // {x:?, y:?} 
+  let position = setToRandom(200); 
+
+  // Add image to div id = game 
+  let game = document.getElementById('game'); 
+  let newimg = document.createElement('img'); 
+  newimg.style.position = 'absolute'; 
+  newimg.src = './images/PacMan1.png'; 
+  newimg.width = 100;
+
+  // TODO: set position here
+  newimg.style.top = position.y;
+  newimg.style.left = position.x;
+
+  // TODO add new Child image to game
+  game.appendChild(newimg);
+
+  // return details in an object
+  return {
+    position,
+    velocity,
+    newimg,
+  };
+}
+
+function update() {
+  // loop over pacmen array and move each one and move image in DOM
+  pacMen.forEach((item) => {
+    checkCollisions(item);
+    item.position.x += item.velocity.x;
+    item.position.y += item.velocity.y;
+
+    item.newimg.style.left = item.position.x;
+    item.newimg.style.top = item.position.y;
+    console.log(item.newimg.style.left);
+    console.log(item.newimg.style.top);
+
+  });
+  setTimeout(update, 20);
+}
+
+function checkCollisions(item) {
+  if (item.newimg.width + item.position.x > window.innerWidth ||
+    item.position.x < 0) {
+    item.velocity.x = -(item.velocity.x);
+  }
+
+  if (item.newimg.height + item.position.y > window.innerHeight || 
+    item.position.y < 0) {
+    item.velocity.y = -(item.velocity.y);
+  } 
+}
+
+function makeOne() {
+  pacMen.push(makePac()); // add a new PacMan
+}
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = { checkCollisions, update, pacMen };
+}
